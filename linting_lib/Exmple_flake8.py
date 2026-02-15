edafd761e9f3612cffce01f104309b1a090cc54b
DEVWORK2025 <mydevw@gmail.com>
2026-02-15 14:08:40 +0530

    Add files via upload

---
+++ linting_lib/Exmple_flake8.py
@@ -0,0 +1,12 @@
+import random,sys
+import os
+
+def myfun():
+ ran= random.random()
+ return random.randint(0,100)
+def multiply  (a,b):
+    return a*b
+
+
+
+print(multiply(myfun(),myfun()))
---
+++ linting_lib/first_lint_details.py
@@ -0,0 +1,13 @@
+class employee:
+    def __init__(s,emp_id):
+        self.emp_id = emp_id
+        print('inside the class')
+
+shiva = employee('1234')
+
+
+def meetings(name1,name2):
+   name1.details = 'nitin'
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
+meetings(employee('sumit'),shiva)
